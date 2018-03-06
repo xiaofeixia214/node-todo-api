27ab559bc22b68e1259e9901eb2ce5fb136e8aa3
Ruofei Xie <ruofeixie@DBGPRX-Ruofei-Xie.local>
2018-03-05 21:49:16 -0800

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -56,6 +57,32 @@ app.delete('/todos/:id', (req, res) => {
 		}
 		res.send({todo});
 	}).catch((e) => {
+		res.status(400).send();
+	});
+});
+
+app.patch('/todos/:id', (req, res) => {
+	const id = req.params.id;
+	const body = _.pick(req.body, ['text', 'completed']);
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	if (_.isBoolean(body.completed) && body.completed) {
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+	}
+
+	Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+		if (!todo) {
+			res.send(404).send();
+		}
+		res.send({todo});
+	}).catch((e) => {
+		console.log(e);
 		res.status(400).send();
 	});
 });
