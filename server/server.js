b241f5766f206c877ceec460b0fc4644b75fe289
Ruofei Xie <ruofeixie@DBGPRX-Ruofei-Xie.local>
2018-03-08 15:17:36 -0800

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -89,6 +89,20 @@ app.patch('/todos/:id', (req, res) => {
 	});
 });
 
+app.post('/users', (req, res) => {
+	const body = _.pick(req.body, ['email', 'password']);
+	const user = new User(body);
+
+	user.save().then(() => {
+		return user.generateAuthToken();
+	}).then((token) => {
+		res.header('x-auth', token).send(user);
+	}).catch((e) => {
+		console.log(1, e);
+		res.status(400).send(e);
+	});
+});
+
 app.listen(port, () => {
 	console.log(`Started up at port ${port}`);
 });
