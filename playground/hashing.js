a6368a1c9260fb2378cf21bed2ef197ab2faf070
Ruofei Xie <ruofeixie@DBGPRX-Ruofei-Xie.local>
2018-03-11 17:06:28 -0700

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,28 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-const data = {
-	id: 10
-};
+const password = '123abc!';
 
-const token = jwt.sign(data, '123abc');
-console.log(token);
-const decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+// bcrypt.genSalt(10, (err, salt) => {
+// 	bcrypt.hash(password, salt, (err, hash) => {
+// 		console.log(hash);
+// 	});
+// });
+
+const hashedPassword = '$2a$10$VlD5RD00wBXfNwQb8B8tvu/5YZqfbRrQEYRO3fD.6iyvgPSxI3MYC';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+	console.log(res);
+});
+// const data = {
+// 	id: 10
+// };
+
+// const token = jwt.sign(data, '123abc');
+// console.log(token);
+// const decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 
 // const message = 'I am user number 3';
