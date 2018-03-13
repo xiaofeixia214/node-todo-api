5e72f391bfd7be629e12761c5af80f6cdd421a3f
Ruofei Xie <ruofeixie@DBGPRX-Ruofei-Xie.local>
2018-03-12 22:33:06 -0700

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -50,6 +50,15 @@ UserSchema.methods.generateAuthToken = function() {
 	});
 };
 
+UserSchema.methods.removeToken = function(token) {
+	const user = this;
+	return user.update({
+		$pull: {
+			tokens: {token}
+		}
+	});
+};
+
 UserSchema.statics.findByToken = function(token) {
 	const User = this;
 	let decoded;
