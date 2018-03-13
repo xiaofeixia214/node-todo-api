ed9cadedd07ff2d7a73340e80b82cd08e35b258b
Ruofei Xie <ruofeixie@DBGPRX-Ruofei-Xie.local>
2018-03-12 17:24:24 -0700

    Add tests for GET /users/me and POST users

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-require('./config/config.js');
+require('./config/config');
 
 const _ = require('lodash');
 const express = require('express');
@@ -11,7 +11,7 @@ const {User} = require('./models/user');
 const {authenticate} = require('./middleware/authenticate');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
