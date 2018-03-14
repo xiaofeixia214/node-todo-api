00ea6a271e901cc01cf4cbf955d667b870a4271a
Ruofei Xie <ruofeixie@DBGPRX-Ruofei-Xie.local>
2018-03-14 14:08:08 -0700

    Update config to use json file

--- server/models/user.js
+++ server/models/user.js
@@ -42,7 +42,7 @@ UserSchema.methods.toJSON = function() {
 UserSchema.methods.generateAuthToken = function() {
 	const user = this;
 	const access = 'auth';
-	const token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+	const token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
 	user.tokens = user.tokens.concat([{access, token}]);
 	return user.save().then(() => {
@@ -63,7 +63,7 @@ UserSchema.statics.findByToken = function(token) {
 	const User = this;
 	let decoded;
 	try {
-		decoded = jwt.verify(token, 'abc123');
+		decoded = jwt.verify(token, process.env.JWT_SECRET);
 	} catch(e) {
 		return Promise.reject();
 	}
